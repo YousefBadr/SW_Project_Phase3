15c5d5b7dfc7b9ba2baf1f6bf5433497776ac260
YousefBadr <45340990+YousefBadr@users.noreply.github.com>
2018-12-23 23:12:34 +0200

    Add files via upload

--- SW_Project/src/Posts_model.java
+++ SW_Project/src/Posts_model.java
@@ -1,4 +1,42 @@
+import java.util.Vector;
+
+//import SWE.Post;
 
 public class Posts_model {
-
+	
+	public static Vector<Post>Posts_Database=new Vector<Post>();
+	
+	public String Add_Post_To_Model(Post post)
+	{
+		Posts_Database.add(post);
+		return "Done !";
+		
+	}
+	
+	
+	public Post Get_Post_By_ID(String Post_ID)
+	{
+		return null;
+		
+	}
+	public Vector Search_Posts(String Mail)
+	{
+		return null;
+		
+	}
+	public Boolean Set_PI_Delivery_Value(String Confirm , String Post_ID)
+	{
+		return null;
+		
+	}
+	public Vector Get_cat_Posts(String Category)
+	{
+		return null;
+		
+	}
+	public String Delete_POst(String ID)
+	{
+		return ID;
+		
+	}
 }
--- SW_Project/src/Report.java
+++ SW_Project/src/Report.java
@@ -1,4 +1,19 @@
 
 public class Report {
+	public String USer_ID;
+	public String Post_ID;
+	public String report;
+	public String Report_ID;
 
+	public Boolean New_Report(String Disc , String User_id , String Post_id)
+	{
+		return null;
+		
+	}
+	public void Inform_Dev(String report_id)
+	{
+		
+	
+
+}
 }
